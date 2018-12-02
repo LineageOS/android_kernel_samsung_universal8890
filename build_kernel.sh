eecff31b8bcf8a1714e565f9a26771c296e2523c
Wirusx64 <wirusmod@gmail.com>
2018-12-02 22:01:03 +0100

    mac802.11 injection patch

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,7 +11,7 @@ BUILD_ROOT_DIR=$BUILD_KERNEL_DIR/..
 BUILD_KERNEL_OUT_DIR=$BUILD_ROOT_DIR/kernel_out/JESSE_KERNEL_OBJ
 PRODUCT_OUT=$BUILD_ROOT_DIR/kernel_out
 
-BUILD_CROSS_COMPILE=/home/wirus/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+BUILD_CROSS_COMPILE=/home/wirusx/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 # Default Python version is 2.7
